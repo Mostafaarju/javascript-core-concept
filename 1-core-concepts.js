78fc72b562c413eaf067eec514d9ada8589b36cd
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2021-02-21 09:40:52 +0100

    JavaScript Core Concept

---
+++ 1-core-concepts.js
@@ -0,0 +1,55 @@
+// Variable: let, const
+let money = 20;
+money = 12;
+
+const name = 'kudus';
+
+let isConfident = false;
+
+const person = {
+    name: 'kudus',
+    age: 30,
+    city: 'Dhaka'
+}
+const statement = `This Person ${name} with age ${person.age}`;
+
+// condition
+if (money > 20 && age >= 45) {
+
+} else {
+
+}
+
+// Array
+const number = [22, 31, 44, 52];
+const respected = ['Motiur Rahman', 'Abdur Roife', 'Jahangir Alom'];
+const product = [{
+    name: 'Mobile',
+    Price: 80
+}, {
+    name: 'Tablet',
+    price: 70
+}, {
+    name: 'Computer',
+    price: 100
+}];
+
+// Loop: for loop
+for (let i = 0; i < respect.length; i++) {
+    const element = respect[i];
+
+}
+
+// function: Regular, Arrow
+function add(num1, numb2) {
+    return num1 + numb2;
+}
+const result = add(45, 50);
+
+// Arrow
+const fiveTime = num => num * 5;
+const add2 = (num1, num2) => num1 + num2;
+const doMath = (x, y) => {
+    const sum = x + y;
+    return sum * 2;
+}
